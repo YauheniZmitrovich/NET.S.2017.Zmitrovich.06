b362d7baa4fe8b354a559980aec26602cdd81afd
zmitr-gen <zmitr-gen@yandex.ru>
2017-07-11 18:03:17 +0300

    Was added implementation of "Bubble sort algorithm" for jagged int[] array. All was covered by unit tests.

---
+++ Logic.Tests/IntArrSortingTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Logic.IntArrSorting;
+using NUnit.Framework;
+
+namespace Logic.Tests
+{
+
+    [TestFixture]
+    public class IntArrSortingTests
+    {
+
+        int[][] jaggedArray = new int[][]
+        {
+            new int[] {1,3,5,7,9},
+            new int[] {11,22},
+            new int[] {0,2,4,6}
+        };
+
+        [Test]
+        public void BubbleSort_AskSortingBySumOfMembers_ReturnsSortedArray()
+        {
+            int[][] arr=new int[3][];
+            jaggedArray.CopyTo(arr, 0);
+            int[][] expectedArray = new int[][]
+            {
+                new int[] {0,2,4,6},
+                new int[] {1,3,5,7,9},
+                new int[] {11,22}
+            };
+
+            IntArrSorting.BubbleSortByRows(arr, ArrayComparisonBy.SumOfMembers, OrderBy.Asc);
+
+            Assert.AreEqual(arr, expectedArray);
+        }
+
+        [Test]
+        public void BubbleSort_DescSortingByMaxMember_ReturnsSortedArray()
+        {
+            int[][] arr = new int[3][];
+            jaggedArray.CopyTo(arr, 0);
+            int[][] expectedArray = new int[][]
+            {
+                new int[] {11,22},
+                new int[] {1,3,5,7,9},
+                new int[] {0,2,4,6},       
+            };
+
+            IntArrSorting.BubbleSortByRows(arr, ArrayComparisonBy.MaxMember, OrderBy.Desc);
+
+            Assert.AreEqual(arr, expectedArray);
+        }
+
+        [Test]
+        public void BubbleSort_DescSortingByMin_ReturnsSortedArray()
+        {
+            int[][] arr = new int[3][];
+            jaggedArray.CopyTo(arr, 0);
+            int[][] expectedArray = new int[][]
+            {
+                new int[] {0,2,4,6},
+                new int[] {1,3,5,7,9},
+                new int[] {11,22},
+            };
+
+            IntArrSorting.BubbleSortByRows(arr, ArrayComparisonBy.MinMember, OrderBy.Asc);
+
+            Assert.AreEqual(arr, expectedArray);
+        }
+
+        [Test]
+        public void BubbleSort_EmptyString_ThrowsArgumentException()
+        {
+            int[][] arr = new int[][]
+            {
+                new int[] {0,2,4,6},
+                new int[] {},
+                new int[] {11,22},
+            };
+
+            Assert.Catch<ArgumentException>(()=> 
+            IntArrSorting.BubbleSortByRows(arr, ArrayComparisonBy.MinMember, OrderBy.Asc));
+        }
+
+        [Test]
+        public void BubbleSort_NullString_ThrowsArgumentNullException()
+        {
+            int[][] arr = new int[3][];
+            arr[0] = new int[] { 1, 2 };
+
+            Assert.Catch <ArgumentNullException>(() =>
+            IntArrSorting.BubbleSortByRows(arr, ArrayComparisonBy.MinMember, OrderBy.Asc));
+        }
+  
+    }
+
+}
+
+
---
+++ Logic/IntArrSorting.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public static class IntArrSorting
+    {
+
+        #region Enums
+
+        /// <summary>
+        /// Ascending or descending ordering.
+        /// </summary>
+        public enum OrderBy { Asc, Desc }
+
+        /// <summary>
+        /// Types of comparison of two int[] arrays.
+        /// </summary>
+        public enum ArrayComparisonBy { SumOfMembers, MaxMember, MinMember }
+
+        #endregion
+
+
+        #region Sorting algorithms
+
+        /// <summary>
+        /// Bubble sorting for jagged int[][] array by rows.
+        /// </summary>
+        /// <param name="arr"> Jagged int[][] array. </param>
+        /// <param name="arrComparisonBy"> Type of comparison two rows. </param>
+        /// <param name="orderBy"> Ascending or descending ordering. </param>
+        public static void BubbleSortByRows(int[][] arr, ArrayComparisonBy arrComparisonBy, OrderBy orderBy)
+        {
+            CheckInputArray(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (Compare(arr[j], arr[j + 1], arrComparisonBy, orderBy) > 0)
+                    {
+                        int[] temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+        }
+
+        #endregion
+
+
+        #region Private
+
+        /// <summary>
+        ///  Compares two int[] arrays  objects and returns an integer that indicates
+        ///  their relative position in the sort order.
+        /// </summary>
+        /// <param name="arr1"> The first array to compare. </param>
+        /// <param name="arr2"> The second array to compare. </param>
+        /// <param name="arrComparisonBy"> How we compare our arrays. </param>
+        /// <returns>
+        ///  A 32-bit signed integer that indicates relationship between the two
+        ///  comparands. Value Condition Less than zero arr1 is less than arr2. Zero arr1 equals
+        ///  arr2. Greater than zero arr1 is greater than arr2.
+        private static int Compare(int[] arr1, int[] arr2, ArrayComparisonBy arrComparisonBy, OrderBy orderBy)
+        {
+            int res = 0;
+            switch (arrComparisonBy)
+            {
+                case ArrayComparisonBy.SumOfMembers:
+                    res = arr1.Sum() - arr2.Sum();
+                    return (orderBy == OrderBy.Asc) ? res : -res;
+
+                case ArrayComparisonBy.MaxMember:
+                    res = arr1.Max() - arr2.Max();
+                    return (orderBy == OrderBy.Asc) ? res : -res;
+
+                case ArrayComparisonBy.MinMember:
+                    res = arr1.Min() - arr2.Min();
+                    return (orderBy == OrderBy.Asc) ? res : -res;
+            }
+
+            return 0;
+        }
+
+        private static void CheckInputArray(int[][] arr)
+        {
+            if (arr == null)
+                throw new ArgumentNullException();
+
+            if (arr.Length == 0)
+                throw new ArgumentException();
+
+            foreach (int[] internArr in arr)
+            {
+                if (internArr == null)
+                    throw new ArgumentNullException();
+                if (internArr.Length == 0)
+                    throw new ArgumentException();
+            }
+        }
+
+        #endregion
+
+    }
+}
